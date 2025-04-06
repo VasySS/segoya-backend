bc1b7a612364ecc882a72ae1b4f700dc6a402d20
VasyS <rdy4future@gmail.com>
2025-04-06 18:55:50 +0300

    feat: exclude /openapi requests from middleware logger

--- internal/controller/http/middleware/logger.go
+++ internal/controller/http/middleware/logger.go
@@ -12,7 +12,7 @@ import (
 // Logger is a middleware for logging requests.
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/health" {
+		if r.URL.Path == "/health" || strings.HasPrefix(r.URL.Path, "/openapi") {
 			next.ServeHTTP(w, r)
 			return
 		}
